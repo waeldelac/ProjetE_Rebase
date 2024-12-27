75f2acb7ae43112b42e0d7e520f1ecdd9eab903f
wael <wael.delacroix@gmail.com>
2024-12-28 00:10:06 +0100

    feat: transmutate impl + bug fix and opti

---
+++ Source/MyProjectTest/blobActor.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "blobActor.h"
+
+
+// Sets default values
+AblobActor::AblobActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	BlobComponent = CreateDefaultSubobject<UBlobComponent>(TEXT("BlobComponent"));
+	BlobComponent->SetIsReplicated(true);
+	BlobComponent->setElementConfig(E_ELEMENT::EARTH, E_PLAYABLE::NORMAL, E_MAPPED_AUTHORIZATION::ALL_ALLOWED, 100);
+	BlobComponent->setElementConfig(E_ELEMENT::WATER, E_PLAYABLE::NORMAL, E_MAPPED_AUTHORIZATION::ALL_ALLOWED, 100);
+	BlobComponent->setElementConfig(E_ELEMENT::FIRE, E_PLAYABLE::NORMAL, E_MAPPED_AUTHORIZATION::ALL_ALLOWED, 100);
+	BlobComponent->setElementConfig(E_ELEMENT::AIR, E_PLAYABLE::NORMAL, E_MAPPED_AUTHORIZATION::ALL_ALLOWED, 100);
+
+}
+
+// Called when the game starts or when spawned
+void AblobActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AblobActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/MyProjectTest/blobActor.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BlobComponent.h"
+#include "BlobInterface.h"
+#include "blobActor.generated.h"
+
+UCLASS()
+class MYPROJECTTEST_API AblobActor : public AActor, public IBlobInterface
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UBlobComponent* BlobComponent;
+	
+public:	
+	// Sets default values for this actor's properties
+	AblobActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	UFUNCTION(BlueprintCallable, Category = SomeCategory)
+	UBlobComponent* getBlobComponent() { return BlobComponent; };
+	UFUNCTION(BlueprintCallable, Category = SomeCategory)
+	AActor* getActorOfBlob() override { return Cast<AActor>(this); };
+	virtual void Tick(float DeltaTime) override;
+
+	
+	
+};
